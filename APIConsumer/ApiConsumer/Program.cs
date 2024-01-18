9a27bc6204a504a08912cab255063583c7ed5de6
Arildo <arildodraix@gmail.com>
2024-01-18 08:29:55 -0300

    Project main files of dynamic consumer apis

---
+++ APIConsumer/ApiConsumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace ApiConsumer
+{
+
+    public class ApiConsumerMain
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task<string> GetResponseApi(string urlApi)
+        {
+            try
+            {
+                SetupHttpClient(urlApi);
+                
+                HttpResponseMessage response = await client.GetAsync(urlApi);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonString = await response.Content.ReadAsStringAsync();
+
+                    return jsonString;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error accessing the API: {e.Message}");
+            }
+
+            return null;
+        }
+
+        private static void SetupHttpClient(string urlApi)
+        {
+            client.BaseAddress = new Uri(urlApi);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
---
+++ APIConsumer/MainCallApis/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using ApiConsumer;
+using MainCallApis;
+using System.Collections;
+using System.Globalization;
+using System.Resources;
+
+class Program
+{
+    static async Task Main()
+    {
+        ResourceSet? resourceSet = SetupApis();
+        await CallApis(resourceSet);
+    }
+
+    static async Task CallApis(ResourceSet? resourceSet)
+    {
+        if (resourceSet != null)
+        {
+            foreach (DictionaryEntry entry in resourceSet)
+            {
+                var response = await ApiConsumerMain.GetResponseApi(entry.Value.ToString());
+
+                if (response != null)
+                {
+                    Console.WriteLine($"Response from {entry.Value} is:" + Environment.NewLine + $"{response}" + Environment.NewLine + Environment.NewLine);
+                }
+               
+            }
+        }
+    
+    }
+
+    static ResourceSet? SetupApis()
+    {
+        var culture = CultureInfo.InvariantCulture;
+        var resourceManager = Resources.ResourceManager;
+        return resourceManager.GetResourceSet(culture, createIfNotExists: true, tryParents: true);
+    }
+
+}
